5b5bab3dffa1ec562c2daf1dece705f5deab0ff5
admin <manoj.t@3ktechnologies.com>
2023-09-27 14:10:20 +0530

    my  second commit

---
+++ src/test/java/javaprg/BarcodeGenarater.java
@@ -0,0 +1,49 @@
+package javaprg;
+
+
+	import java.awt.image.BufferedImage;
+	import java.io.File;
+	import java.io.IOException;
+	import javax.imageio.ImageIO;
+	import com.google.zxing.BarcodeFormat;
+	import com.google.zxing.EncodeHintType;
+	import com.google.zxing.MultiFormatWriter;
+	import com.google.zxing.common.BitMatrix;
+
+	public class BarcodeGenarater {
+
+	    public static void main(String[] args) {
+	        String barcodeData = "123456789"; // The data you want to encode
+	        String filePath = "output.png"; // Output file path
+
+	        int width = 300; // Width of the barcode
+	        int height = 100; // Height of the barcode
+
+	        try {
+	            // Encode the data into a BitMatrix
+	            BitMatrix bitMatrix = new MultiFormatWriter().encode(
+	                barcodeData, BarcodeFormat.CODE_128, width, height,
+	                // Encoding hints can be specified here, but are optional
+	                null
+	            );
+
+	            // Convert the BitMatrix to a BufferedImage
+	            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	            for (int x = 0; x < width; x++) {
+	                for (int y = 0; y < height; y++) {
+	                    image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+	                }
+	            }
+
+	            // Save the image to a file
+	            File outputFile = new File(filePath);
+	            ImageIO.write(image, "png", outputFile);
+
+	            System.out.println("Barcode saved to: " + filePath);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+
+
